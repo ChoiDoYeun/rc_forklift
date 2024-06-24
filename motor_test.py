5c03378dd8beed39dc77eb0fbfc4cd9e108cbba8
ChoiDoYeun <fff0803@naver.com>
2024-06-24 23:02:01 +0900

    motor test and pin setup

--- motor_test.py
+++ motor_test.py
@@ -1,52 +1,58 @@
 import RPi.GPIO as GPIO
 import time
 
-# 핀 번호 설정 (BCM 모드)
-EN = 18
-IN1 = 17
-IN2 = 27
+class MotorController:
+    def __init__(self, en, in1, in2):
+        self.en = en
+        self.in1 = in1
+        self.in2 = in2
+        GPIO.setup(self.en, GPIO.OUT)
+        GPIO.setup(self.in1, GPIO.OUT)
+        GPIO.setup(self.in2, GPIO.OUT)
+        self.pwm = GPIO.PWM(self.en, 100)
+        self.pwm.start(0)
+        
+    def forward(self, speed):
+        self.pwm.ChangeDutyCycle(speed)
+        GPIO.output(self.in1, GPIO.HIGH)
+        GPIO.output(self.in2, GPIO.LOW)
 
-# GPIO 모드 설정
+    def backward(self, speed):
+        self.pwm.ChangeDutyCycle(speed)
+        GPIO.output(self.in1, GPIO.LOW)
+        GPIO.output(self.in2, GPIO.HIGH)
+
+    def stop(self):
+        self.pwm.ChangeDutyCycle(0)
+        GPIO.output(self.in1, GPIO.LOW)
+        GPIO.output(self.in2, GPIO.LOW)
+
+    def cleanup(self):
+        self.pwm.stop()
+
 GPIO.setmode(GPIO.BCM)
 
-# 모터 핀 설정
-GPIO.setup(EN, GPIO.OUT)
-GPIO.setup(IN1, GPIO.OUT)
-GPIO.setup(IN2, GPIO.OUT)
-
-# PWM 설정: EN 핀에 PWM 신호를 보냄
-pwm = GPIO.PWM(EN, 100)  # 100 Hz 주파수
-pwm.start(0)  # PWM 시작, 0% 듀티 사이클
-
-def motor_forward(speed):
-    pwm.ChangeDutyCycle(speed)  # 모터 속도 설정
-    GPIO.output(IN1, GPIO.HIGH)
-    GPIO.output(IN2, GPIO.LOW)
-    print("모터 전진")
-
-def motor_backward(speed):
-    pwm.ChangeDutyCycle(speed)
-    GPIO.output(IN1, GPIO.LOW)
-    GPIO.output(IN2, GPIO.HIGH)
-    print("모터 후진")
-
-def motor_stop():
-    pwm.ChangeDutyCycle(0)
-    GPIO.output(IN1, GPIO.LOW)
-    GPIO.output(IN2, GPIO.LOW)
-    print("모터 정지")
+motor1 = MotorController(18, 17, 27)
+motor2 = MotorController(22, 23, 24)
+motor3 = MotorController(12,5,6)
+motor4 = MotorController(16,13,26)
 
 try:
-    # 테스트 순서
-    motor_forward(50)  # 50% 속도로 전진
-    time.sleep(2)      # 2초간 동작
-    motor_backward(50) # 50% 속도로 후진
-    time.sleep(2)
-    motor_stop()       # 모터 정지
+    motor1.backward(50)
+    motor2.backward(50)
+    motor3.backward(50)
+    motor4.backward(50)
+    time.sleep(3)
+    motor1.forward(50)
+    motor2.forward(50)
+    motor3.forward(50)
+    motor4.forward(50)
+    time.sleep(3)
 
 except KeyboardInterrupt:
-    pass
+    print("Interrupted by user")
 
 finally:
-    pwm.stop()         # PWM 정지
-    GPIO.cleanup()     # GPIO 핀 설정 초기화
+    motor1.cleanup()
+    motor2.cleanup()
+    GPIO.cleanup()
