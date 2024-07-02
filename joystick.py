677d2823868fb9d3154627cbf893235626234f65
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-07-02 21:00:27 +0900

    Update joystick.py

--- joystick.py
+++ joystick.py
@@ -72,13 +72,13 @@ try:
                     motor4.backward(40)
                 elif event.key == pygame.K_a:
                     motor1.stop()  # 좌측 앞 모터 속도 낮춤
-                    motor2.forward(70)  # 우측 앞 모터 속도 높임
+                    motor2.forward(80)  # 우측 앞 모터 속도 높임
                     motor3.stop()  # 좌측 뒤 모터 속도 낮춤
-                    motor4.forward(70)  # 우측 뒤 모터 속도 높임
+                    motor4.forward(80)  # 우측 뒤 모터 속도 높임
                 elif event.key == pygame.K_d:
-                    motor1.forward(70)  # 좌측 앞 모터 속도 높임
+                    motor1.forward(80)  # 좌측 앞 모터 속도 높임
                     motor2.stop()      # 우측 앞 모터 멈춤
-                    motor3.forward(40)  # 좌측 뒤 모터 속도 높임
+                    motor3.forward(80)  # 좌측 뒤 모터 속도 높임
                     motor4.stop()      # 우측 뒤 모터 멈춤
                 elif event.key == pygame.K_s:
                     stop_motors()
