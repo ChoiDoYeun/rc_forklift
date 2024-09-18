ff9b04c56d4ad37d0c19950086b1eca009c2e2ce
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-09-18 20:45:18 +0900

    Update motor_test.py

--- motor_test.py
+++ motor_test.py
@@ -1,8 +1,10 @@
 import RPi.GPIO as GPIO
 import time
-from adafruit_servokit import ServoKit
-import pygame
 
+# GPIO 설정
+GPIO.setmode(GPIO.BCM)
+
+# 모터 제어 클래스
 class MotorController:
     def __init__(self, en, in1, in2):
         self.en = en
@@ -11,102 +13,60 @@ class MotorController:
         GPIO.setup(self.en, GPIO.OUT)
         GPIO.setup(self.in1, GPIO.OUT)
         GPIO.setup(self.in2, GPIO.OUT)
-        self.pwm = GPIO.PWM(self.en, 100)
+        self.pwm = GPIO.PWM(self.en, 100)  # PWM 주파수 100Hz
         self.pwm.start(0)
-        
-    def set_speed(self, speed):
+
+    def forward(self, speed):
+        GPIO.output(self.in1, GPIO.HIGH)
+        GPIO.output(self.in2, GPIO.LOW)
         self.pwm.ChangeDutyCycle(speed)
 
     def backward(self, speed):
-        self.set_speed(speed)
-        GPIO.output(self.in1, GPIO.HIGH)
-        GPIO.output(self.in2, GPIO.LOW)
-
-    def forward(self, speed):
-        self.set_speed(speed)
         GPIO.output(self.in1, GPIO.LOW)
         GPIO.output(self.in2, GPIO.HIGH)
+        self.pwm.ChangeDutyCycle(speed)
 
     def stop(self):
-        self.set_speed(0)
         GPIO.output(self.in1, GPIO.LOW)
         GPIO.output(self.in2, GPIO.LOW)
+        self.pwm.ChangeDutyCycle(0)
 
-    def cleanup(self):
-        self.pwm.stop()
+# 모터 초기화 (왼쪽 모터: motor1, 오른쪽 모터: motor2)
+motor1 = MotorController(18, 17, 27)  # 모터1: en(18), in1(17), in2(27)
+motor2 = MotorController(22, 23, 24)  # 모터2: en(22), in1(23), in2(24)
 
-GPIO.setmode(GPIO.BCM)
-
-motor1 = MotorController(18, 17, 27)
-motor2 = MotorController(22, 23, 24)
-motor3 = MotorController(12, 5, 6)
-motor4 = MotorController(16, 13, 26)
-
-# PCA9685 모듈 초기화 (16채널 서보 컨트롤러 사용)
-kit = ServoKit(channels=16)
-
-# 서보모터 초기 설정
-kit.servo[0].angle = 0
-kit.servo[1].angle = 150
-kit.servo[2].angle = 0
-kit.servo[3].angle = 0
-kit.servo[4].angle = 0
-
-# Pygame 초기화
-pygame.init()
-screen = pygame.display.set_mode((100, 100))
-
-def stop_motors():
-    motor1.stop()
-    motor2.stop()
-    motor3.stop()
-    motor4.stop()
-
+# 모터 테스트
 try:
-    print("W: Motor1 Forward, S: Motor1 Backward, E: Motor2 Forward, D: Motor2 Backward")
-    print("R: Motor3 Forward, F: Motor3 Backward, T: Motor4 Forward, G: Motor4 Backward, Q: Quit")
-    
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w:
-                    motor1.forward(70)
-                elif event.key == pygame.K_s:
-                    motor1.backward(70)
-                elif event.key == pygame.K_e:
-                    motor2.forward(70)
-                elif event.key == pygame.K_d:
-                    motor2.backward(70)
-                elif event.key == pygame.K_r:
-                    motor3.forward(70)
-                elif event.key == pygame.K_f:
-                    motor3.backward(70)
-                elif event.key == pygame.K_t:
-                    motor4.forward(70)
-                elif event.key == pygame.K_g:
-                    motor4.backward(70)
-                elif event.key == pygame.K_q:
-                    running = False
-            elif event.type == pygame.KEYUP:
-                if event.key in [pygame.K_w, pygame.K_s]:
-                    motor1.stop()
-                elif event.key in [pygame.K_e, pygame.K_d]:
-                    motor2.stop()
-                elif event.key in [pygame.K_r, pygame.K_f]:
-                    motor3.stop()
-                elif event.key in [pygame.K_t, pygame.K_g]:
-                    motor4.stop()
+    # 모터1 전진 테스트
+    print("모터1 전진")
+    motor1.forward(50)  # 50% 속도로 전진
+    time.sleep(2)
 
-except KeyboardInterrupt:
-    print("Interrupted by user")
+    # 모터1 후진 테스트
+    print("모터1 후진")
+    motor1.backward(50)  # 50% 속도로 후진
+    time.sleep(2)
+
+    # 모터1 정지
+    print("모터1 정지")
+    motor1.stop()
+    time.sleep(1)
+
+    # 모터2 전진 테스트
+    print("모터2 전진")
+    motor2.forward(50)  # 50% 속도로 전진
+    time.sleep(2)
+
+    # 모터2 후진 테스트
+    print("모터2 후진")
+    motor2.backward(50)  # 50% 속도로 후진
+    time.sleep(2)
+
+    # 모터2 정지
+    print("모터2 정지")
+    motor2.stop()
+    time.sleep(1)
 
 finally:
-    motor1.cleanup()
-    motor2.cleanup()
-    motor3.cleanup()
-    motor4.cleanup()
+    # GPIO 정리
     GPIO.cleanup()
-    pygame.quit()
