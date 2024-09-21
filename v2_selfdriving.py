58773f1279137f0788bd11d023c80375ce86962e
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-09-21 18:24:12 +0900

    Update v2_selfdriving.py

--- v2_selfdriving.py
+++ v2_selfdriving.py
@@ -84,7 +84,7 @@ class selfdrivingCNN(nn.Module):
 
 # 모델 로드
 model = selfdrivingCNN().to(device)
-model.load_state_dict(torch.load('pruned_0921_newtrack.pth', map_location=device))
+model.load_state_dict(torch.load('0921_newtrack.pth', map_location=device))
 model.eval()
 
 # 서보모터 설정
